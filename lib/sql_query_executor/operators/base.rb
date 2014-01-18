c6654aa0699faf2154788d438709ec1c6e5649b7
Caio Torres <efreesen@gmail.com>
2014-01-18 17:34:42 -0200

    Refactoring, simplifying Operators::Base#convert_value method

--- lib/sql_query_executor/operators/base.rb
+++ lib/sql_query_executor/operators/base.rb
@@ -22,24 +22,41 @@ module SqlQueryExecutor
       end
 
       def convert_value(value)
-        array = []
+        return value.to_i if is_number?(value)
 
-        if value.size >= 10
-          array = value.split(/ |:/)
-          array[0] = array.first.split('-')
-          array = array.flatten
-        end
+        methods = {3 => "convert_date", 7 => "convert_time"}
 
-        if (value.to_i.to_s == value.to_s)
-          return value.to_i
-        elsif array.size == 3
-          return array.first.to_i > 1000 ? Date.new(array[0].to_i, array[1].to_i, array[2].to_i) : value
-        elsif array.size == 7
-          array[6] = array[6] == '+0000' ? '+00:00' : array[6].gsub('00', ':00')
-          return array.first.to_i > 1000 ? Time.new(*array) : value
-        end
+        array = split(value)
+  
+        value = (send(methods[array.size], array) || value) if methods.keys.include?(array.size)
 
         value
+      end
+
+    private
+      def is_number?(value)
+        (value.to_i.to_s == value.to_s)
+      end
+
+      def split(value)
+        return [] unless value.size >= 10
+
+        array = value.split(/[ :]/)
+        array[0] = array.first.split('-')
+        array.flatten
+      end
+
+      def convert_date(args)
+        return if args.first.to_i < 1000
+        
+        Date.new(args[0].to_i, args[1].to_i, args[2].to_i)
+      end
+
+      def convert_time(args)
+        return if args.first.to_i < 1000
+        args[6] = args[6].gsub('00', ':00').gsub("+:", "+")
+        
+        Time.new(*args)
       end
     end
   end
