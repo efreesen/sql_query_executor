f2ba646a6b1bab75fd65071e97631a3d4cf8f460
Caio Torres <efreesen@gmail.com>
2014-03-08 15:48:21 -0300

    Removed Query::Base and moved the behavior to Base class

--- lib/sql_query_executor/base.rb
+++ lib/sql_query_executor/base.rb
@@ -1,13 +1,45 @@
 require 'ostruct'
-require 'sql_query_executor/query/base'
+require 'sql_query_executor/query/query_normalizer'
 require 'sql_query_executor/query/sub_query'
 
 # Simulates a SQL where clause to filter objects from the database
 module SqlQueryExecutor #:nodoc:
   class Base #:nodoc:
-    def initialize(collection=[])
-      @collection = []
+    STRING_SPACE    = "$SS$"
+    QUERY_SPACE     = "$QS$"
+    TEMP_SPACE      = "$TS$"
 
+    def initialize(collection, query)
+      query = query.first if query.respond_to?(:size) && query.size == 1 && !query.is_a?(Hash)
+
+      message = check_query(query)
+      raise ArgumentError.new(message) if message
+
+      get_collection(collection)
+      query = Query::QueryNormalizer.execute(query)
+      @query = SqlQueryExecutor::Query::SubQuery.new query, @collection
+    end
+
+    def execute!
+      @query.execute!
+    end
+
+    def to_sql
+      @query.to_sql
+    end
+
+    def selector
+      @query.selector
+    end
+
+    # Recursive method that divides the query in sub queries, executes each part individually
+    # and finally relates its results as specified in the query.
+    def self.where(collection, *query)
+      self.new(collection, query).execute!
+    end
+
+  private
+    def get_collection(collection)
       if collection.any? && collection.first.is_a?(Hash)
         convert_collection(collection)
       else
@@ -15,20 +47,8 @@ module SqlQueryExecutor #:nodoc:
       end
     end
 
-    # Recursive method that divides the query in sub queries, executes each part individually
-    # and finally relates its results as specified in the query.
-    def where(*query)
-      query = query.first if query.respond_to?(:size) && query.size == 1
-
-      message = check_query(query)
-      raise ArgumentError.new(message) if message
-
-      query = SqlQueryExecutor::Query::Base.new(query, @collection)
-      query.execute!
-    end
-
-  private
     def convert_collection(collection)
+      @collection = []
       collection.each do |object|
         attributes = object.is_a?(Hash) ? object : object.attributes
         register = OpenStruct.new(attributes)
--- spec/sql_query_executor/base_spec.rb
+++ spec/sql_query_executor/base_spec.rb
@@ -1,527 +1,91 @@
 require 'spec_helper'
 
 require 'sql_query_executor'
+require 'sql_query_executor/base'
 
-describe SqlQueryExecutor, "Base" do
-  before do
-    @data = []
-
-    @data << {id: 1, name: "US",     language: 'English'}
-    @data << {id: 2, name: "Canada", language: 'English', monarch: "The Crown of England"}
-    @data << {id: 3, name: "Mexico", language: 'Spanish'}
-    @data << {id: 4, name: "UK",     language: 'English', monarch: "The Crown of England"}
-    @data << {id: 5, name: "Brazil", founded_at: Time.parse('1500-04-22 13:34:25')}
-  end
-
-  subject { SqlQueryExecutor::Base.new(@data) }
-
-  describe "initialize" do
-    before do
-      class Model
-        attr_reader :attributes
-
-        def initialize(attributes)
-          @attributes = attributes
-        end
-
-        def id
-          @attributes[:id]
-        end
-      end
-
-      class Collection
-        def initialize(collection)
-          @collection = []
-
-          collection.each do |hash|
-            @collection << Model.new(hash)
-          end
-        end
-
-        def all
-          @collection
-        end
-      end
-    end
-
-    subject { SqlQueryExecutor::Base.new(Collection.new(@data).all) }
-
-    it 'initializes with a conforming collection' do
-      expect(subject.where(id: 1).first.attributes).to eq (@data.first)
-    end
-  end
-
+describe SqlQueryExecutor::Base do
   describe ".where" do
-    context "when invalid query is passed" do
-      it "raises an ArgumentError" do
-        query = [{name: "John"}]
-
-        expect { subject.where(query) }.to raise_error(ArgumentError, "First element from array must be a String. eg: [\"name = ?\", \"John\"]")
-      end
+    let(:data) do
+      [
+        {id: 1, name: "US",     language: 'English'},
+        {id: 2, name: "Canada", language: 'English', monarch: "The Crown of England"},
+        {id: 3, name: "Mexico", language: 'Spanish'},
+        {id: 4, name: "UK",     language: 'English', monarch: "The Crown of England"},
+        {id: 5, name: "Brazil", founded_at: Time.parse('1500-04-22 13:34:25')}
+      ]
     end
 
-    describe "=" do
-      context "when attribute is string" do
-        it "matches a record" do
-          record = subject.where("name = 'US'")
-          record.size.should == 1
-          record.first.id.should == 1
-          record.first.name.should == 'US'
+    context 'conforming collection' do
+      before do
+        class Model
+          attr_reader :attributes
+
+          def initialize(attributes)
+            @attributes = attributes
+          end
+
+          def id
+            @attributes[:id]
+          end
         end
 
-        it "doesn't match any record" do
-          record = subject.where("name = 'Argentina'")
-          record.count.should == 0
-          record.should == []
-        end
-      end
+        class Collection
+          def initialize(collection)
+            @collection = []
 
-      context "when attribute is integer" do
-        it "matches a record" do
-          record = subject.where("id = 1")
-          record.count.should == 1
-          record.first.id.should == 1
-          record.first.name.should == 'US'
-        end
+            collection.each do |hash|
+              @collection << Model.new(hash)
+            end
+          end
 
-        it "doesn't match any record" do
-          record = subject.where("id = 43")
-          record.count.should == 0
-          record.should == []
+          def all
+            @collection
+          end
         end
       end
 
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at = ?", Time.parse('1500-04-22 13:34:25'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
+      it 'initializes with a conforming collection' do
+        expect(described_class.where(Collection.new(data).all, id: 1).first.attributes).to eq (data.first)
+      end
+      
+      context "when invalid query is passed" do
+        it "raises an ArgumentError" do
+          query = [{name: "John"}]
 
-        it "doesn't match any record" do
-          record = subject.where("id = ?", Time.parse('1500-09-07 13:34:25'))
-          record.count.should == 0
-          record.should == []
+          expect { described_class.where(Collection.new(data).all, query) }.to raise_error(ArgumentError, "First element from array must be a String. eg: [\"name = ?\", \"John\"]")
         end
       end
     end
 
-    describe ">" do
-      context "when attribute is a string" do
-        it "matches a record" do
-          records = subject.where("name > 'T'")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[3]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("name > 'Z'")
-          record.count.should == 0
-          record.should == []
-        end
+    context 'not conforming collection' do
+      it 'initializes with a conforming collection' do
+        expect(described_class.where(data, id: 1)).to eq([OpenStruct.new(data.first)])
       end
+      
+      context "when invalid query is passed" do
+        it "raises an ArgumentError" do
+          query = [{name: "John"}]
 
-      context "when attribute is integer" do
-        it "matches a record" do
-          records = subject.where("id > 3")
-          records.count.should == 2
-          records.first.id.should == 4
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[3], @data[4]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("id > 5")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at > ?", Time.parse('1500-04-20 13:34:25'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("founded_at > ?", Time.parse('1500-04-23 13:34:25'))
-          record.count.should == 0
-          record.should == []
+          expect { described_class.where(data, query) }.to raise_error(ArgumentError, "First element from array must be a String. eg: [\"name = ?\", \"John\"]")
         end
       end
     end
 
-    describe ">=" do
-      context "when attribute is a string" do
-        it "matches a record" do
-          records = subject.where("name >= 'U'")
-          records.count.should == 2
-          records.first.id.should == 1
+    context 'selector' do
+      let(:query)    { 'id > 3' }
+      let(:selector) { {'id' => {'$gt' => 3}} }
 
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[3]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("name >= 'Z'")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is integer" do
-        it "matches a record" do
-          records = subject.where("id >= 4")
-          records.count.should == 2
-          records.first.id.should == 4
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[3], @data[4]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("id >= 6")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at >= ?", Time.parse('1500-04-22 13:34:25'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("founded_at >= ?", Time.parse('1500-04-23 13:34:25'))
-          record.count.should == 0
-          record.should == []
-        end
+      it 'converts query' do
+        expect(described_class.new([], query).selector).to eq selector
       end
     end
 
-    describe "<" do
-      context "when attribute is a string" do
-        it "matches a record" do
-          records = subject.where("name < 'C'")
-          records.count.should == 1
-          records.first.id.should == 5
+    context 'to_sql' do
+      let(:query)    { "name = 'Brazil'" }
+      let(:selector) { {name: 'Brazil'} }
 
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[4]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("name < 'B'")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is integer" do
-        it "matches a record" do
-          records = subject.where("id < 3")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[1]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("id < 1")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at < ?", Time.parse('1500-04-22 13:34:26'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("founded_at < ?", Time.parse('1500-04-22 13:34:25'))
-          record.count.should == 0
-          record.should == []
-        end
-      end
-    end
-
-    describe "<=" do
-      context "when attribute is a string" do
-        it "matches a record" do
-          records = subject.where("name <= 'Brb'")
-          records.count.should == 1
-          records.first.id.should == 5
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[4]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("name <= 'A'")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is integer" do
-        it "matches a record" do
-          records = subject.where("id <= 2")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[1]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("id <= 0")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at <= ?", Time.parse('1500-04-22 13:34:25'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("founded_at <= ?", Time.parse('1500-04-22 13:34:24'))
-          record.count.should == 0
-          record.should == []
-        end
-      end
-    end
-
-    describe "between" do
-      context "when attribute is a string" do
-        it "matches a record" do
-          records = subject.where("name between 'A' and 'C'")
-          records.count.should == 1
-          records.first.id.should == 5
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[4]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("name between 'K' and 'M'")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is integer" do
-        it "matches a record" do
-          records = subject.where("id between 1 and 2")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[1]]
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("id between 6 and 10")
-          record.count.should == 0
-          record.should == []
-        end
-      end
-
-      context "when attribute is datetime" do
-        it "matches a record" do
-          record = subject.where("founded_at between ? and ?", Time.parse('1500-04-22 13:34:24'), Time.parse('1500-04-22 13:34:26'))
-          record.count.should == 1
-          record.first.id.should == 5
-          record.first.name.should == 'Brazil'
-        end
-
-        it "doesn't match any record" do
-          record = subject.where("founded_at between ? and ?", Time.parse('1500-04-22 13:34:26'), Time.parse('1500-09-22 13:34:25'))
-          record.count.should == 0
-          record.should == []
-        end
-      end
-    end
-
-    describe "is" do
-      it "attribute is condition" do
-        records = subject.where("founded_at is null")
-        records.count.should == 4
-        records.first.id.should == 1
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[0], @data[1], @data[2], @data[3]]
-      end
-
-      it "attribute is not condition" do
-        id = @data.last[:id]
-        records = subject.where("founded_at is not null")
-        records.count.should == 1
-        records.first.id.should == id
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[id-1]]
-      end
-    end
-
-    describe "and" do
-      it "attribute and condition" do
-        records = subject.where("language = 'English' and monarch = 'The Crown of England'")
-        records.count.should == 2
-        records.first.id.should == 2
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[1], @data[3]]
-      end
-
-      it "integer attribute and condition" do
-        records = subject.where("id = 2 and language = 'English'")
-        records.count.should == 1
-        records.first.id.should == 2
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[1]]
-      end
-    end
-
-    describe "or" do
-      it "attribute or condition" do
-        records = subject.where("language = 'English' or language = 'Spanish'")
-        records.count.should == 4
-        records.first.id.should == 1
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[0], @data[1], @data[2], @data[3]]
-      end
-
-      context "nested queries" do
-        it "respects priority" do
-          records = subject.where("(language = 'English' and name = 'US') or (language is null)")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[4]]
-        end
-
-        it "respects priority" do
-          records = subject.where("(language is null) or (language = 'English' and name = 'US')")
-          records.count.should == 2
-          records.first.id.should == 1
-
-          records.map! do |record|
-            record.to_h
-          end
-
-          records.should == [@data[0], @data[4]]
-        end
-      end
-    end
-
-    describe "in" do
-      it "attribute in condition" do
-        records = subject.where("language in ('English', 'Spanish')")
-        records.count.should == 4
-        records.first.id.should == 1
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[0], @data[1], @data[2], @data[3]]
-      end
-
-      xit "attribute not in condition" do
-        records = subject.where("language not in ('English', 'Spanish')")
-        records.count.should == 1
-        records.first.id.should == 5
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[4]]
-      end
-    end
-
-    describe "not" do
-      xit "attribute not condition" do
-        records = subject.where("not language = 'English'")
-        records.count.should == 1
-        records.first.id.should == 5
-
-        records.map! do |record|
-          record.to_h
-        end
-
-        records.should == [@data[4]]
+      it 'converts selector' do
+        expect(described_class.new([], selector).to_sql).to eq query
       end
     end
   end
